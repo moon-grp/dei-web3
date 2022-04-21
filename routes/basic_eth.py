09d3b28eebfff3edb1aa78540aee7242f1ad70b7
emohex <olumidemm@gmail.com>
2022-04-21 22:31:11 +0100

    lesson 1 done

--- routes/basic_eth.py
+++ routes/basic_eth.py
@@ -1,8 +1,39 @@
 from fastapi import APIRouter
-
+from web3 import Web3
+from pydantic import BaseModel
+from dotenv import load_dotenv
 basicEth = APIRouter()
+import os
+load_dotenv()
 
 
-@basicEth.get("/hello", tags=["basic eth"])
+infuraUrl = os.getenv("IUrl")
+#infuraUrl = "https://mainnet.infura.io/v3/e9d8cbfbc4a74312a35a971807c58fd3"
+
+
+
+class ethInput(BaseModel):
+    addr: str
+    
+
+@basicEth.get("/get", tags=["basic eth"])
 async def hello():
-    print("helloooo")
+    web3 = Web3(Web3.HTTPProvider(infuraUrl))
+    check  = web3.isConnected()
+    print (check)
+    
+
+@basicEth.get("/latestblock", tags=["basic eth"])
+async def get_latest_block():
+    web3 = Web3(Web3.HTTPProvider(infuraUrl))
+    latestBlock  = web3.eth.blockNumber
+    print (latestBlock)
+
+
+@basicEth.post("/getaddressbalance", tags=["basic eth"])
+async def get_address_balance(details:ethInput):
+    web3 = Web3(Web3.HTTPProvider(infuraUrl))
+    balance  = web3.eth.getBalance(details.addr)
+    balanceInWEi = web3.fromWei(balance, "ether")
+    print (balance, balanceInWEi)
+   
